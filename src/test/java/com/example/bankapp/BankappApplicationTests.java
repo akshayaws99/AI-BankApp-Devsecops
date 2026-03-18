a3398c399bb6e167a3ab8d4cea7981bae5fefb64
akshaysolanki <akshayaws99@gmail.com>
2026-03-18 18:10:43 +0530

    updated java file

--- src/test/java/com/example/bankapp/BankappApplicationTests.java
+++ src/test/java/com/example/bankapp/BankappApplicationTests.java
@@ -3,12 +3,11 @@ package com.example.bankapp;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = {
-    "spring.datasource.url=jdbc:h2:mem:testdb",
-    "spring.datasource.driverClassName=org.h2.Driver",
-    "spring.datasource.username=sa",
-    "spring.datasource.password=",
-    "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
-})
+@SpringBootTest
+class BankappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
 
 }
